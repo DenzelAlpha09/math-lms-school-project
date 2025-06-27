9f034f77bd6cdae12ebf125f6e21a1bb576863d5
Denzel Ashitey <denzelniiashitey@gmail.com>
2025-06-27 09:47:57 +0000

    login bug fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const logInBtn = document.querySelector(".logInBtn");
 const loginDialog = document.querySelector(".dark-bg");
 const closeBtn = document.querySelector(".close");
+const logInButton = document.querySelector(".logIn-btn");
 
 logInBtn.addEventListener("click", () => {
   loginDialog.style.display = "flex";
@@ -8,4 +9,13 @@ logInBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   loginDialog.style.display = "none";
+});
+
+logInButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const indexNumber = document.querySelector(".index-number").value;
+  const password = document.querySelector(".password").value;
+  if (indexNumber == "12345678" && password == "12345678") {
+    location.assign("./pages/dashboard.html");
+  }
 });
